ba85a766eaa0e932a1ed2baf14fbe17ddef20924
Yusuf <15554011@firat.edu.tr>
2019-02-16 21:21:34 +0200

    16.02.2019 22:21

--- resources/views/admin/mainStructure/navbar.blade.php
+++ resources/views/admin/mainStructure/navbar.blade.php
@@ -19,8 +19,6 @@
                 <i class="fas fa-user-circle fa-fw"></i>
             </a>
             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
-                <a class="dropdown-item" href="#">{{ __('general.settings') }}</a>
-                <div class="dropdown-divider"></div>
                 <a class="dropdown-item" href="{{ route('admin.logout') }}">{{ __('general.logout') }}</a>
             </div>
         </li>
